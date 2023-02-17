eeabd266e1c67f15ccd76cb08b3bc176b76e8853
Ricardo7173 <123588637+Ricardo7173@users.noreply.github.com>
2023-02-17 14:58:04 -0600

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component:AppComponent, pathMatch:'full'},
-  {path:'login', component:LoginComponent, pathMatch:'full'},
-  {path:'register', component:RegisterComponent, pathMatch: 'full'}
-];  
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
